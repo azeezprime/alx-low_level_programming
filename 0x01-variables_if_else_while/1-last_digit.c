6ce5a6f3a05cdf8afe8477a056ea896f8535d929
azeezprime <tosinonix8@gmail.com>
2022-07-02 09:53:18 -0700

    2nd commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entrypoint
+ *
+ * Return: Alaways 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 5)
+		printf("%d and is greater then 5\n", n);
+	else if (n == 0)
+		printf("%d and is 0\n", n);
+	else if (n < 6)
+		printf("%d and is less than 6 and not 0\n"n);
+
+return (0);
