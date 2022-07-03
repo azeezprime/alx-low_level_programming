dc66911467ada3d388f54cdf5a8046c1463c3eab
azeezprime <tosinonix8@gmail.com>
2022-07-03 14:27:27 -0700

    2nd commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,6 +18,7 @@ int main(void)
 	else if (n == 0)
 		printf("%d and is 0\n", n);
 	else if (n < 6)
-		printf("%d and is less than 6 and not 0\n"n);
+		printf("%d and is less then 6 and not 0\n", n);
 
 return (0);
+}
